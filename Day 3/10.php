8425f20bc614a3f91f2fcc66af1741b855d207d3
mahiraj-chavda-2000 <80085241+mahiraj-chavda-2000@users.noreply.github.com>
2021-06-11 21:32:39 +0530

    Update 10.php

--- Day 3/10.php
+++ Day 3/10.php
@@ -1,3 +1,17 @@
 <?php
-print_r($_POST);
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'mdahir');
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+$member=$_POST['member'];
+$pr=$_POST['pr'];
+$m=$member['0'];
+$p=$pr['0'];
+$sql = "INSERT INTO `mahi` ( `member`,`pr`) VALUES ('$m','$p')";
+
+$result = mysqli_query($conn, $sql);
 ?>
