399e5cc376474ef1dc71dbc9737103a022dfa85a
mahiraj-chavda-2000 <80085241+mahiraj-chavda-2000@users.noreply.github.com>
2021-06-12 10:20:34 +0530

    Update 10.php

--- Day 3/10.php
+++ Day 3/10.php
@@ -7,11 +7,18 @@ $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-$member=$_POST['member'];
-$pr=$_POST['pr'];
-$m=$member['0'];
-$p=$pr['0'];
-$sql = "INSERT INTO `mahi` ( `member`,`pr`) VALUES ('$m','$p')";
+// print_r($_POST);
+$m=$_POST['member'];
+$p=$_POST['pr'];
+$stmt = $conn->prepare("INSERT INTO mahi ( member,pr) VALUES (?, ?)");
+$stmt->bind_param("ss", $member, $pr);
 
-$result = mysqli_query($conn, $sql);
+for ($x = 0; $x < count($_POST); $x++) {
+    $member = $m[$x];
+    $pr = $p[$x];
+    $stmt->execute();
+    // printf("Error: %s.\n", mysqli_stmt_error($stmt));
+  }
+  $stmt->close();
+  $conn->close();
 ?>
