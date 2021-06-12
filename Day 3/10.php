40e2a71319b4c348ee1442a893fec2e303ed9608
mahiraj-chavda-2000 <80085241+mahiraj-chavda-2000@users.noreply.github.com>
2021-06-12 14:54:29 +0530

    Update 10.php

--- Day 3/10.php
+++ Day 3/10.php
@@ -14,10 +14,11 @@ $stmt = $conn->prepare("INSERT INTO mahi ( member,pr) VALUES (?, ?)");
 $stmt->bind_param("ss", $member, $pr);
 
 for ($x = 0; $x < count($_POST); $x++) {
-    $member = $m[$x];
+  if ($m[$x]!=null && $p[$x]!=null) {
+   $member = $m[$x];
     $pr = $p[$x];
     $stmt->execute();
-    // printf("Error: %s.\n", mysqli_stmt_error($stmt));
+  } 
   }
   $stmt->close();
   $conn->close();
